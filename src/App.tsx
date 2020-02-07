20688687ff596405f0ce1ee12b3492a2c3791a8e
Julien Tregoat <julien@offchainlabs.com>
2020-02-07 14:10:11 -0500

    start storing config params. get web3 format the form createRollup fn

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,31 @@ import {
   ButtonProps,
   ButtonGroup,
   Card,
+  Form,
   FormControl,
   InputGroup
 } from 'react-bootstrap'
+import { ethers } from 'ethers'
+import { web3Injected, getInjectedWeb3 } from './util/web3'
+
+const ROLLUP_FACTORY = '0xd309F6Ba1B53CbDF9c0690eD1316A347eBb7adf9'
+const WALLET_IDX = 0
+
+interface RollupChainConfig {
+  gracePeriod: string
+  arbGasSpeed: string
+  maxSteps: string
+  maxTimeWidth: string
+  stakeRequirement: string
+}
+
+const configLocal: RollupChainConfig = {
+  gracePeriod: '450',
+  arbGasSpeed: '20000000',
+  maxSteps: '1000000000',
+  maxTimeWidth: '20',
+  stakeRequirement: '1'
+}
 
 function readFileAsync(file: File): Promise<Uint8Array> {
   return new Promise<Uint8Array>((resolve, reject) => {
@@ -45,8 +67,86 @@ const groupButtonStyle: ButtonProps = {
   variant: 'info'
 }
 
+declare global {
+  interface EventTarget {
+    value: any
+  }
+}
+
+const FormattedFormInput: React.FC<{
+  type?: string
+  onChange: React.FormEventHandler<HTMLInputElement>
+}> = ({ children, onChange, type = 'text' }) => (
+  <InputGroup>
+    <InputGroup.Prepend className={styles.formLabel}>
+      <InputGroup.Text className={styles.formLabelText}>
+        {children}
+      </InputGroup.Text>
+    </InputGroup.Prepend>
+    <Form.Control onChange={onChange} />
+  </InputGroup>
+)
+
 const App = () => {
   const { getRootProps, getInputProps } = useDropzone({ accept: '.ao' })
+  const [web3, setWeb3] = React.useState<ethers.providers.JsonRpcProvider>()
+  const [factory, setFactory] = React.useState<ArbFactory>()
+  const [config, setConfig] = React.useState<Partial<RollupChainConfig>>({})
+  console.log(config)
+
+  React.useEffect(() => {
+    if (!web3) {
+      if (!web3Injected(window.ethereum)) {
+        alert('web3 not present') // TODO
+      } else {
+        getInjectedWeb3().then(provider => {
+          setWeb3(provider)
+          setFactory(
+            ArbFactoryFactory.connect(
+              ROLLUP_FACTORY,
+              provider.getSigner(WALLET_IDX)
+            )
+          )
+        })
+      }
+    }
+  })
+
+  const handleCreateRollup = async () => {
+    if (!web3 || !factory) {
+      throw new Error('missing web3 or factory')
+    }
+
+    // TODO better form validation
+    const {
+      gracePeriod,
+      arbGasSpeed,
+      maxSteps,
+      maxTimeWidth,
+      stakeRequirement
+    } = config
+    if (
+      !gracePeriod ||
+      !arbGasSpeed ||
+      !maxSteps ||
+      !maxTimeWidth ||
+      !stakeRequirement
+    ) {
+      throw new Error('missing required rollup parameter ' + JSON.stringify(config))
+    }
+
+    const addresses = await web3.listAccounts()
+
+    factory.createRollup(
+      ethers.constants.HashZero,
+      ethers.utils.bigNumberify(gracePeriod),
+      ethers.utils.bigNumberify(arbGasSpeed),
+      ethers.utils.bigNumberify(maxSteps),
+      ethers.utils.bigNumberify(maxTimeWidth),
+      ethers.utils.bigNumberify(stakeRequirement),
+      addresses[WALLET_IDX]
+    )
+  }
 
   return (
     <div className={styles.rootContainer}>
@@ -64,63 +164,78 @@ const App = () => {
         </Card>
       </div>
 
-      <div className={mergeStyles(styles.baseTitle, styles.subtitle)}>
-        Chain Parameters
+      <div>
+        <div className={mergeStyles(styles.baseTitle, styles.subtitle)}>
+          Chain Configuration
+        </div>
+
+        <div className={styles.presetsContainer}>
+          <span>Presets</span>
+          <ButtonGroup>
+            <Button {...groupButtonStyle}>Blank</Button>
+            <Button {...groupButtonStyle}>Local testing</Button>
+          </ButtonGroup>
+        </div>
+
+        <div className={styles.chainParamsForm}>
+          <InputGroup>
+            <InputGroup.Prepend className={styles.formLabel}>
+              <InputGroup.Text className={styles.formLabelText}>
+                Machine Hash
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl
+              className={styles.machineHash}
+              type={'text'}
+              readOnly
+              plaintext
+              value={'TODO'}
+            />
+          </InputGroup>
+
+          <FormattedFormInput
+            children={'Stake requirement (ETH)'}
+            onChange={e => {
+              const stakeRequirement = e.target.value
+              setConfig(c => ({ ...c, stakeRequirement }))
+            }}
+          />
+          <FormattedFormInput
+            children={'Grace period (seconds)'}
+            onChange={e => {
+              const gracePeriod = e.target.value
+              setConfig(c => ({ ...c, gracePeriod }))
+            }}
+          />
+          <FormattedFormInput
+            children={'Arb gas speed'}
+            onChange={e => {
+              const arbGasSpeed = e.target.value
+              setConfig(c => ({ ...c, arbGasSpeed }))
+            }}
+          />
+          <FormattedFormInput
+            children={'Max Steps'}
+            onChange={e => {
+              const maxSteps = e.target.value
+              setConfig(c => ({ ...c, maxSteps }))
+            }}
+          />
+          <FormattedFormInput
+            children={'Max time bounds width (blocks)'}
+            onChange={e => {
+              const maxTimeWidth = e.target.value
+              setConfig(c => ({ ...c, maxTimeWidth }))
+            }}
+          />
+        </div>
       </div>
 
-      <div className={styles.presetsContainer}>
-        <span>Presets</span>
-        <ButtonGroup>
-          <Button {...groupButtonStyle}>Blank</Button>
-          <Button {...groupButtonStyle}>Local testing</Button>
-        </ButtonGroup>
-      </div>
-
-      <div className={styles.chainParamsForm}>
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Machine Hash</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl type={'text'} />
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Stake requirement (ETH)</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl type={'text'} />
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Grace period (seconds)</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl type={'text'} />
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Arb Gas Speed</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl type={'text'} />
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Max steps</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl type={'text'} />
-        </InputGroup>
-
-        <InputGroup>
-          <InputGroup.Prepend>
-            <InputGroup.Text>Max time bounds width (blocks)</InputGroup.Text>
-          </InputGroup.Prepend>
-          <FormControl type={'text'} />
-        </InputGroup>
-      </div>
-
-      <Button variant={'primary'} className={styles.createButton}>
+      <Button
+        variant={'primary'}
+        className={styles.createButton}
+        onClick={handleCreateRollup}
+      >
         Create Rollup Chain
       </Button>
     </div>
