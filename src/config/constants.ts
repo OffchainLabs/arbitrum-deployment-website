d15902abd75aa3dd6590b7b9b7bf9dd37df71fcb
Harry Kalodner <harry@offchainlabs.com>
2020-05-05 22:57:53 -0400

    Update rollup factories

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -6,7 +6,7 @@ export const DEV_DOC_URL =
   'https://developer.offchainlabs.com/docs/Chain_parameters/'
 // seems like metamask switched rinkeby and kovan chain ids?
 export const ROLLUP_FACTORIES: { [chainId: number]: string | undefined } = {
-  3: '0x670dFfa13bF5fF7A42C94EA94A9484Dd7C6a1650', // ropsten
-  4: '0x2ff2D1Cced0EBD48ca829d3C9E7f86A1141F761F', // rinkeby
-  42: '0x46168bA7Cc052427085544534623fDEcb9bD92D8' // kovan
+  3: '0x0501be99fc97F352D5bA9bFaC23a3d4eEbDF7030', // ropsten
+  4: '0x0501be99fc97F352D5bA9bFaC23a3d4eEbDF7030', // rinkeby
+  42: '0x0501be99fc97F352D5bA9bFaC23a3d4eEbDF7030' // kovan
 }
